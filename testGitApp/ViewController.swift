5b92bd261b94e43a53e147e2731fdc8bc52ec1d5
Tung Fam <tung@pixpie.co>
2016-12-13 23:49:20 +0200

    Initial Commit

---
+++ testGitApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testGitApp
+//
+//  Created by Tung Fam on 12/13/16.
+//  Copyright © 2016 Tung Fam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
